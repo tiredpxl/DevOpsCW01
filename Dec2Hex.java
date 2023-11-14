6375d15b7add4e253a067be3d0afa7b326d24e1f
Ubuntu <ubuntu@ip-172-31-92-27.ec2.internal>
2023-11-14 18:49:49 +0000

    changed output message

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -21,7 +21,7 @@ class Dec2Hex
             num= num/16;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        System.out.println("The Decimal Value " + num + " has a Hexadecimal representation of: " + hexadecimal);
 
     }
 }
