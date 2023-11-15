09908040cd642cb036337b772a95001c1d41abb2
Ubuntu <ubuntu@ip-172-31-85-225.ec2.internal>
2023-11-15 00:00:44 +0000

    error message for no argument

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,6 +7,12 @@ class Dec2Hex
 
     public static void main(String args[])
     {
+
+	if (args.length == 0) {
+	System.out.println("Error: Please provide a decimal number");
+	return;
+	}
+
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
