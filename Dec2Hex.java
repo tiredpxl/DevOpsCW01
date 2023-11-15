7f0c28a55aee8311e33caa620fa4a241c63175dc
Ubuntu <ubuntu@ip-172-31-80-11.ec2.internal>
2023-11-15 01:43:18 +0000

    added error detection for non integer inputs

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -13,6 +13,14 @@ class Dec2Hex
 	return;
 	}
 
+	try{
+	Arg1 = Integer.parseInt(args[0]);
+	}
+	catch(Exception e) {
+	System.out.println("Error: Not a valid integer");
+	return;
+	}
+
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
